919512144041392a430e952c9bb49cc96575ed22
RiosCoder <edsonrios2010@gmail.com>
2025-10-04 11:51:28 -0400

    Adicion de servicio para country

--- src/app/country/by-country-page/by-country-page.component.ts
+++ src/app/country/by-country-page/by-country-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { SearchInputComponent } from "../search-input/search-input.component";
 import { CountryListComponent } from "../country-list/country-list.component";
 import { CountryService } from '../services/country.service';
+import { throwError } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-country-page',
@@ -13,6 +15,8 @@ export class ByCountryPageComponent implements OnInit {
   countryService = inject(CountryService)
   place: string = 'por pais';
   recepcionMensaje: string = "";
+  listaCountry: Country[] = [];
+  isError: string | null = null;
 
   ngOnInit(): void {
     console.log('onInit by-capital');
@@ -22,6 +26,15 @@ export class ByCountryPageComponent implements OnInit {
 
   onSearch(mensaje: string) {
     console.log('miraaaa: ', mensaje);
-
+    this.countryService.searchByCountry(mensaje)
+      .subscribe({
+        next: ((next) => {
+          console.log('next: ', next);
+          this.listaCountry = next;
+        }),
+        error: ((err) => {
+          this.isError = err;
+        })
+      });
   }
 }
